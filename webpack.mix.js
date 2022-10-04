d25cf1beecbab1d405abecf05075a1d5b9b0304e
mark <markpackham1@gmail.com>
2022-10-04 12:51:17 +0100

    remove code and prepare to downgrade to laravel 8

--- webpack.mix.js
+++
@@ -1,9 +0,0 @@
-const mix = require("laravel-mix");
-
-mix.js("resources/js/app.js", "public/js")
-    .extract()
-    .vue(3)
-    .postCss("resources/css/app.css", "public/css", [
-        //
-    ])
-    .version();
