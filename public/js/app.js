4dd32f2a0b7b3836e6fae85873d793c0f2d63cd1
mark <markpackham1@gmail.com>
2022-10-07 16:40:51 +0100

    create standard sqlite database
    https://youtu.be/QyqrYdhSku0?t=5154

--- public/js/app.js
+++ public/js/app.js
@@ -552,6 +552,14 @@ var map = {
 	"./Users/Create.vue": [
 		"./resources/js/Pages/Users/Create.vue",
 		"resources_js_Pages_Users_Create_vue"
+	],
+	"./Users/Index": [
+		"./resources/js/Pages/Users/Index.vue",
+		"resources_js_Pages_Users_Index_vue"
+	],
+	"./Users/Index.vue": [
+		"./resources/js/Pages/Users/Index.vue",
+		"resources_js_Pages_Users_Index_vue"
 	]
 };
 function webpackAsyncContext(req) {
