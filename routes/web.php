86698b7ca0790bda3957f44f32be2cb86eb8c639
mark <markpackham1@gmail.com>
2022-10-08 08:38:15 +0100

    create LoginController
    https://youtu.be/QyqrYdhSku0?t=8898

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,18 @@
 <?php
 
-use App\Models\User;
-use Inertia\Inertia;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('login', [LoginController::class, 'create'])->name('login');
+Route::post('login', [LoginController::class, 'store']);
+Route::post('logout', [LoginController::class, 'destroy'])->middleware('auth');
 
-Route::inertia('/', 'Home');
-Route::inertia('/settings', 'Settings');
+Route::middleware('auth')->group(function () {
+    Route::inertia('/', 'Home');
+    Route::inertia('/settings', 'Settings');
 
-Route::get('/users', [UsersController::class, 'index']);
-Route::get('/users/create', [UsersController::class, 'create']);
-Route::post('/users', [UsersController::class, 'store']);
-
-Route::post('/logout', function () {
-    dd('Logging user out');
+    Route::get('/users', [UsersController::class, 'index']);
+    Route::get('/users/create', [UsersController::class, 'create']);
+    Route::post('/users', [UsersController::class, 'store']);
 });
