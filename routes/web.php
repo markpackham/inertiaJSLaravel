2548f366d7783605776deb46f4c74889b2a619b4
mark <markpackham1@gmail.com>
2022-10-08 22:38:59 +0100

    create UserPolicy
    https://youtu.be/QyqrYdhSku0?t=9926

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\UsersController;
+use App\Models\User;
+use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersController;
+use App\Http\Controllers\Auth\LoginController;
 
 Route::get('login', [LoginController::class, 'create'])->name('login');
 Route::post('login', [LoginController::class, 'store']);
@@ -12,7 +14,44 @@ Route::middleware('auth')->group(function () {
     Route::inertia('/', 'Home');
     Route::inertia('/settings', 'Settings');
 
-    Route::get('/users', [UsersController::class, 'index']);
+    Route::get('/users', function () {
+        return Inertia::render('Users/Index', [
+            'users' => User::query()
+                ->when(Request::input('search'), function ($query, $search) {
+                    $query->where('name', 'like', "%{$search}%");
+                })
+                ->paginate(10)
+                ->withQueryString()
+                ->through(fn ($user) => [
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'can' => [
+                        'edit' => Auth::user()->can('edit', $user)
+                    ]
+                ]),
+
+            'filters' => Request::only(['search']),
+            'can' => [
+                'createUser' => Auth::user()->can('create', User::class)
+            ]
+        ]);
+    });
+
+    Route::post('/users', function () {
+        $attributes = Request::validate([
+            'name' => 'required',
+            'email' => ['required', 'email'],
+            'password' => 'required',
+        ]);
+
+        User::create($attributes);
+
+        return redirect('/users');
+    });
+
+
+
+    // Route::get('/users', [UsersController::class, 'index']);
     Route::get('/users/create', [UsersController::class, 'create']);
-    Route::post('/users', [UsersController::class, 'store']);
+    // Route::post('/users', [UsersController::class, 'store']);
 });
